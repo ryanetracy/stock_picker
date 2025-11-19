219a1e2f77b3a9edc3a03db76112f14b6fbb7e1e
ryanetracy <ryanetracy@gmail.com>
2025-11-19 11:35:51 -0500

    build code to preprocess data for sarimax model

--- src/model_preprocess.py
+++ src/model_preprocess.py
@@ -1,6 +1,7 @@
 
 from datetime import datetime
 import polars as pl
+import pandas as pd
 from typing import Tuple
 
 def train_test_split_cutoff(
@@ -16,3 +17,19 @@ def train_test_split_cutoff(
     y_train, y_test = train[label], test[label]
 
     return X_train, X_test, y_train, y_test
+
+def split_ar_on_cutoff(
+    df: pl.DataFrame, cutoff: datetime, label: str, exog_feats: list
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    train = df.filter(pl.col("date") < cutoff)
+    eval = df.filter(pl.col("date") >= cutoff)
+
+    train_pd = train.to_pandas()
+    eval_pd = eval.to_pandas()
+
+    cols_list = [label] + exog_feats
+
+    train_pd = train_pd[cols_list]
+    eval_pd = eval_pd[cols_list]
+
+    return train_pd, eval_pd
