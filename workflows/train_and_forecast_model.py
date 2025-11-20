c3d2ef7e8b4476d164d76c2f4899ff9609911450
ryanetracy <ryanetracy@gmail.com>
2025-11-20 12:50:24 -0500

    update output script with rmse

--- workflows/train_and_forecast_model.py
+++ workflows/train_and_forecast_model.py
@@ -1,4 +1,8 @@
 
+"""
+script for getting all model results.
+"""
+
 from datetime import datetime, date, timedelta
 from dateutil.relativedelta import relativedelta
 from timeit import default_timer as timer
@@ -10,14 +14,14 @@ from src.pipeline import train_and_forecast_xgb
 from src.fit_sarimax_model import *
 
 end_date = datetime.today().strftime("%Y-%m-%d")
-start_date = (datetime.today() - relativedelta(years=5)).strftime("%Y-%m-%d")
+start_date = (datetime.today() - relativedelta(years=10)).strftime("%Y-%m-%d")
 cutoff = (datetime.today() - relativedelta(months=2))
 
 ticker = "AAPL"
 
 timer_xgb_start = timer()
 
-xgb_forecasts, xgb_mse = train_and_forecast_xgb(
+xgb_forecasts, xgb_rmse = train_and_forecast_xgb(
     ticker=ticker,
     start_date=start_date,
     end_date=end_date,
@@ -29,14 +33,14 @@ xgb_forecasts, xgb_mse = train_and_forecast_xgb(
 )
 timer_xgb_end = timer() - timer_xgb_start
 
-print(f"\nXGBoost test MSE on holdout: {xgb_mse:.4f}\n")
+print(f"\nXGBoost test RMSE on holdout: {xgb_rmse:.4f}\n")
 print(f"XGBoost run duration: {timer_xgb_end:.5f} seconds\n\n")
 print(xgb_forecasts)
 print("\n\n")
 
 timer_smax_start = timer()
 
-smax_forecasts, smax_mse = sarimax_wrapper(
+smax_forecasts, smax_rmse = sarimax_wrapper(
     ticker=ticker,
     start_date=start_date,
     end_date=end_date,
@@ -46,7 +50,7 @@ smax_forecasts, smax_mse = sarimax_wrapper(
 )
 timer_smax_end = timer() - timer_smax_start
 
-print(f"\nSARIMAX test MSE on holdout: {smax_mse:.4f}\n")
+print(f"\nSARIMAX test RMSE on holdout: {smax_rmse:.4f}\n")
 print(f"SARIMAX run duration: {timer_smax_end:.5f} seconds\n\n")
 print(smax_forecasts)
 print("\n\n")
